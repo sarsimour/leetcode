2c7344a326237c410da056afaae45fccec40b628
sarsimour <zhu.xiaoming36@gmail.com>
2022-10-24 11:40:44 +0800

    反转链表搞定

--- leetcode/editor/cn/[92]反转链表 II.py
+++ leetcode/editor/cn/[92]反转链表 II.py
@@ -13,16 +13,33 @@ class Solution(object):
         :type right: int
         :rtype: ListNode
         """
+        if left == right:
+            return head
         pre_head = ListNode(0, head)
+        this_node = pre_head
         loc = 0
         left_end = None
-        while loc <= right:
-            if loc == left - 1:
-                left_end = head
-            elif loc > left - 1:
-
-            head = head.next
+        reverse_start = None
+        right_start = None
+        while loc <= right + 1:
+            if loc < left - 1:
+                this_node = this_node.next
+            elif loc == left - 1:
+                left_end = this_node
+                this_node = this_node.next
+            elif loc == left:
+                reverse_start = this_node
+                this_node = this_node.next
+            elif loc > left and loc <= right:
+                temp_next = this_node.next
+                this_node.next = left_end.next
+                left_end.next = this_node
+                reverse_start.next = temp_next
+                this_node = temp_next
+            # else:
+            #     reverse_start.next = this_node
             loc += 1
+        return pre_head.next
 
         
 # leetcode submit region end(Prohibit modification and deletion)
