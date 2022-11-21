e33d79a1e036d5e6ff95f6b3f88f29cca11a4628
sarsimour <zhu.xiaoming36@gmail.com>
2022-11-21 16:41:55 +0800

    34

---
+++ leetcode/editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.py
@@ -0,0 +1,10 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+# leetcode submit region end(Prohibit modification and deletion)
